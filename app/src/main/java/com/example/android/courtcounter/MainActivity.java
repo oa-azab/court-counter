7721cd33ce3a57b9e3c9b5e454837237f5f22ae4
Omar <oa.azab@gmail.com>
2017-02-22 13:17:09 +0200

    Add Team B

--- app/src/main/java/com/example/android/courtcounter/MainActivity.java
+++ app/src/main/java/com/example/android/courtcounter/MainActivity.java
@@ -8,20 +8,23 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     int teamAScore = 0;
+    int teamBScore = 0;
+
+    final int SCORE_FOR_3_POINTS = 3;
+    final int SCORE_FOR_2_POINTS = 2;
+    final int SCORE_FOR_FREE_THROW_POINTS = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
     }
 
     /**
      * Add 3 points to score for Team A.
      */
     public void add3PointTeamA(View view) {
-        teamAScore = teamAScore + 3;
+        teamAScore = teamAScore + SCORE_FOR_3_POINTS;
         displayForTeamA(teamAScore);
     }
 
@@ -29,7 +32,7 @@ public class MainActivity extends AppCompatActivity {
      * Add 2 points to score for Team A.
      */
     public void add2PointTeamA(View view) {
-        teamAScore = teamAScore + 2;
+        teamAScore = teamAScore + SCORE_FOR_2_POINTS;
         displayForTeamA(teamAScore);
     }
 
@@ -37,7 +40,7 @@ public class MainActivity extends AppCompatActivity {
      * Add 1 points to score for Team A.
      */
     public void add1PointTeamA(View view) {
-        teamAScore = teamAScore + 1;
+        teamAScore = teamAScore + SCORE_FOR_FREE_THROW_POINTS;
         displayForTeamA(teamAScore);
     }
 
@@ -46,6 +49,38 @@ public class MainActivity extends AppCompatActivity {
      */
     public void displayForTeamA(int score) {
         TextView scoreView = (TextView) findViewById(R.id.team_a_score);
+        scoreView.setText(String.valueOf(score));
+    }
+
+    /**
+     * Add 3 points to score for Team B.
+     */
+    public void add3PointTeamB(View view) {
+        teamBScore = teamBScore + SCORE_FOR_3_POINTS;
+        displayForTeamB(teamBScore);
+    }
+
+    /**
+     * Add 2 points to score for Team B.
+     */
+    public void add2PointTeamB(View view) {
+        teamBScore = teamBScore + SCORE_FOR_2_POINTS;
+        displayForTeamB(teamBScore);
+    }
+
+    /**
+     * Add 1 points to score for Team B.
+     */
+    public void add1PointTeamB(View view) {
+        teamBScore = teamBScore + SCORE_FOR_FREE_THROW_POINTS;
+        displayForTeamB(teamBScore);
+    }
+
+    /**
+     * Displays the given score for Team B.
+     */
+    public void displayForTeamB(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_b_score);
         scoreView.setText(String.valueOf(score));
     }
 }
