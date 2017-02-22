b4178c6b4c007ba24ebc3237052bb3e79344d380
Omar <oa.azab@gmail.com>
2017-02-22 12:38:54 +0200

    Team A Buttons work

--- app/src/main/java/com/example/android/courtcounter/MainActivity.java
+++ app/src/main/java/com/example/android/courtcounter/MainActivity.java
@@ -2,10 +2,12 @@ package com.example.android.courtcounter;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    int teamAScore = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,4 +17,35 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    /**
+     * Add 3 points to score for Team A.
+     */
+    public void add3PointTeamA(View view) {
+        teamAScore = teamAScore + 3;
+        displayForTeamA(teamAScore);
+    }
+
+    /**
+     * Add 2 points to score for Team A.
+     */
+    public void add2PointTeamA(View view) {
+        teamAScore = teamAScore + 2;
+        displayForTeamA(teamAScore);
+    }
+
+    /**
+     * Add 1 points to score for Team A.
+     */
+    public void add1PointTeamA(View view) {
+        teamAScore = teamAScore + 1;
+        displayForTeamA(teamAScore);
+    }
+
+    /**
+     * Displays the given score for Team A.
+     */
+    public void displayForTeamA(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_a_score);
+        scoreView.setText(String.valueOf(score));
+    }
 }
